126a5227d4e40358494396f22aa807d70e0c6a29
Abdullatif Khayat <abdullatif.khayat123@gmail.com>
2025-01-24 16:13:51 +0300

    adding 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Volunteer from "./pages/Volunteer";
 import Participant from "./pages/Participant";
+import NotFound from "./pages/NotFound";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/volunteer/:id" element={<Volunteer />} />
         <Route path="/participant/:id" element={<Participant />} />
+        <Route path="*" element={<NotFound />} />{" "}
       </Routes>
     </Router>
   );
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+const NotFound = () => {
+  return (
+    <div className="not-found-container">
+      <h1>404 - الصفحة غير متوفرة</h1>
+      <p>
+        الرجاء التواصل مع المدير للحصول على الرابط المطلوب، مع تزويده برقم
+        المستخدم الخاص بك.
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
